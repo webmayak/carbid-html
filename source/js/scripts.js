044f09f08d7f1de9e35260ee4546637264c2c223
Aileen <aileen.kingsler@mail.ru>
2019-08-09 19:23:14 +1000

    Сверстала страницу объявления

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,3 +1,18 @@
 $(function () {
 
+  $('.car-slider__main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.car-slider__nav'
+  });
+  $('.car-slider__nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.car-slider__main',
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true
+  });
+
 });
