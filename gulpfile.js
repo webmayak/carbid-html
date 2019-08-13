6c0094f8fb85ad90d60cd76175139fe74cea10c2
aileen <aileen.kingsler@mail.ru>
2019-08-13 12:53:01 +1000

    На мобильных реализовала показ меню при клике по иконке гамбургера

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/*", gulp.series("copy", "refresh"));
   gulp.watch("source/img/sprite/*.svg", gulp.series("sprite", "html", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/**/*.html", gulp.series("html", "refresh"));
   gulp.watch("source/js/*", gulp.series("scripts", "refresh"));
 });
 
--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,23 @@
 $(function () {
 
+  // mainNav
+  var mainNav = document.querySelector('.main-nav');
+  var mainNavToggle = mainNav.querySelector('.main-nav__toggle');
+
+  mainNav.classList.remove('main-nav--no-js');
+  mainNav.classList.remove('main-nav--opened');
+  mainNav.classList.add('main-nav--closed');
+
+  mainNavToggle.addEventListener('click', function() {
+    if (mainNav.classList.contains('main-nav--closed')) {
+      mainNav.classList.remove('main-nav--closed');
+      mainNav.classList.add('main-nav--opened');
+    } else {
+      mainNav.classList.add('main-nav--closed');
+      mainNav.classList.remove('main-nav--opened');
+    }
+  });
+
   $('.car-slider__main').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
